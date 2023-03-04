2af818ccbaab06e27fe4dd3ebfcc7571b9cd1795
Sudharsan <sudharsan.ssv@gmail.com>
2023-03-04 16:43:43 -0600

    Rudimentary fix to loading files into memory. I think we can improve this, by separating loading code and loading data

--- Spring 2023/CS 5348/proj2/load.cpp
+++ Spring 2023/CS 5348/proj2/load.cpp
@@ -1,7 +1,7 @@
 #include "computer.h"
 
 FILE* Loader::load_prog(char *fname, int base){
-    char delim[] = " ";
+    char delim[] = " \n";
     Memory *mem = returnMemory();
 
     char line[100];
@@ -22,16 +22,29 @@ FILE* Loader::load_prog(char *fname, int base){
     int num;
     int prog_read_counter = 0;
 
+    //load the instructions
+
     for ( int line_counter = 0; line_counter < Ncode + Ndata; line_counter++ ){
         fgets(line, sizeof(line), fp);
+        //cout << endl;
+        //cout << "Need to write: " << line << endl;
         word = strtok(line, delim);
+
         while(word != NULL){
             num = atoi(word);
+
+            //cout << "Writing: " << num << " to " << ( base + prog_read_counter ) << endl;
+
             mem->Mem[ base + prog_read_counter ] = num;
+
+            //cout << "Actual Contents - " << base + prog_read_counter << " : " << mem->Mem[ base + prog_read_counter ] << endl;
+
             prog_read_counter++;
             word = strtok(NULL, delim);
 	      }
     }
+
+    //load the data
 
     return fp;
 
