1add640d66903f5e04b87d6c025685d722e12cc5
Sudharsan <sudharsan.ssv@gmail.com>
2023-03-20 18:46:03 -0500

    Created a Communicator object to store the thread_id, along with a message queue. Each communicator can then manage it's own message_queue in a protected manner and return a reference to it if necessary

--- Spring 2023/CS 5348/test/printer_manager.h
+++ Spring 2023/CS 5348/test/printer_manager.h
@@ -4,10 +4,34 @@
 #include <iostream>
 #include <fstream>
 #include <queue>
+#include <string>
 #include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+class Communicator {
+private:
+    pthread_t tid;
+public:
+    queue<string> *message_queue;
+    Communicator();
+    pthread_t get_tid();
+    void set_tid();
+    void init_message_queue();
+};
 
 pthread_t *communicators;
+Communicator *communicator_references;
 pthread_t printer;
 queue<int> *connection_queue;
+queue<string> **communicator_message_queues;
+
+void spawn_communicators();
+void terminate_communicators();
+void *printer_main(void *PrintingTime);
+void *communicator(void *arg);
+void print_manager_init();
+void printer_manager();
 
 #endif PRINTER_MANAGER_H
