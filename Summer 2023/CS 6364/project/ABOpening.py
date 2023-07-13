a043e04e84c5488bf2fb80b15f9e0e3914acafd4
Sudharsan <sudharsan.ssv@gmail.com>
2023-07-12 22:15:48 -0500

    implemented alpha beta pruning, still need to return the best possible move position

--- Summer 2023/CS 6364/project/ABOpening.py
+++ Summer 2023/CS 6364/project/ABOpening.py
@@ -11,13 +11,16 @@ def main():
 
     #write_board_position_to_file(args[1], flipped_bp)
 
-    final_static_estimation, desirable_move, static_estimation_count = MinMaxAlgorithmOpening(bp, 0, depth)
+    print(sys.maxsize)
+    print( ( -1 * sys.maxsize ) - 1 )
 
-    print_final_static_estimation_data(
-        final_static_estimation, 
-        desirable_move, 
-        static_estimation_count
-    )
+    #final_static_estimation, desirable_move, static_estimation_count = MinMaxAlgorithmOpening(bp, 0, depth)
+
+    # print_final_static_estimation_data(
+    #     final_static_estimation, 
+    #     desirable_move, 
+    #     static_estimation_count
+    # )
 
 def GenerateMovesOpeningBlack(bp):
     black_bp = flip_board_position(bp)
@@ -26,12 +29,11 @@ def GenerateMovesOpeningBlack(bp):
     return [ flip_board_position(black_move) for black_move in black_moves_as_white ]
     
 def MinMaxAlgorithmOpening(bp, level, max_depth):
-    L = GenerateMovesOpening(bp)
-    static_estimates, static_est_count = MaxMin(L, level, max_depth)
-    desirable_move = L[ static_estimates.index(max(static_estimates)) ]
+    static_estimate, static_est_count = ABMaxMin(bp, level, max_depth)
+    desirable_move = L[ static_estimates.index(max(static_estimate)) ]
     return static_est_opening(desirable_move), desirable_move, static_est_count
 
-def MaxMin(L, level, max_depth):
+def ABMaxMin(board_position, alpha, beta, level, max_depth):
     # MaxMin(x, α, β):
     # 1. if x is a leaf return static(x).
     # 2. else do steps 2.1, 2.2, 2.3
@@ -47,24 +49,27 @@ def MaxMin(L, level, max_depth):
         pprint("Evaluating: " + str(L))
 
     if ( level == max_depth ):
-        static_estimates = [ static_est_opening(bp) for bp in L ]
-        return static_estimates, len(static_estimates)
-    
+        return ( static_est_opening(board_position), 1 )
+
     else:
-        level_static_estimations = list()
+        v =  ( -1 * sys.maxsize ) - 1 
         level_static_est_count = 0
-        
+
+        L = GenerateMovesOpening(board_position)
+
         for bp in L:
-            new_L = GenerateMovesOpening(bp)
-            static_estimations, static_est_count = MinMax(new_L, level + 1, max_depth)
-            max_est = max(static_estimations)
-            level_static_est_count += static_est_count
+            min_max_val, static_est_count = ABMinMax(bp, alpha, beta, level + 1, max_depth)
 
-            level_static_estimations.append(max_est)
+            v = max(v, min_max_val)
+            if ( v >= beta ):
+                return v, level_static_est_count + static_est_count
+            else:
+                alpha = max(v, alpha)
+                level_static_est_count += static_est_count
+        
+        return v, static_est_count
 
-        return level_static_estimations, level_static_est_count
-
-def MinMax(L, level, max_depth):
+def ABMinMax(board_position, alpha, beta, level, max_depth):
     # MinMax(x, α, β):
     # 3. if x is a leaf return static(x).
     # 4. else do steps 2.1, 2.2, 2.3
@@ -80,21 +85,23 @@ def MinMax(L, level, max_depth):
         pprint("Evaluating: " + str(L))
 
     if ( level == max_depth ):
-        static_estimates = [ static_est_opening(bp) for bp in L ]
-        return static_estimates, len(static_estimates)
-    
+        return ( static_est_opening(board_position), 1 )
+
     else:
-        level_static_estimations = list()
+        v =  sys.maxsize
         level_static_est_count = 0
-        
+
+        L = GenerateMovesOpening(board_position)
+
         for bp in L:
-            new_L = GenerateMovesOpeningBlack(bp)
-            static_estimations, static_est_count = MaxMin(new_L, level + 1, max_depth)
-            min_est = min(static_estimations)
-            level_static_est_count += static_est_count
-
-            level_static_estimations.append(min_est)
-
-        return level_static_estimations, level_static_est_count
+            max_min_val, static_est_count = ABMaxMin(bp, alpha, beta, level + 1, max_depth)
+            v = min(v, max_min_val)
+            if ( v <= alpha ):
+                return v, level_static_est_count + static_est_count
+            else:
+                beta = min(v, beta)
+                level_static_est_count += static_est_count
+        
+        return v, static_est_count
 
 main()
--- Summer 2023/CS 6364/project/mvg_helper.py
+++ Summer 2023/CS 6364/project/mvg_helper.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from generate import *
 from pprint import pprint
 
