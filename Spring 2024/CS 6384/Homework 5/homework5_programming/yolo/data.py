50efb7d394f6ddb1619ed8aac67a4a8d3c30b3f4
ssudharsan93 <sudharsan.ssv@gmail.com>
2024-04-06 01:25:44 -0500

    Finished problems 1 and 2. Problem 3 remaining

---
+++ Spring 2024/CS 6384/Homework 5/homework5_programming/yolo/data.py
@@ -0,0 +1,234 @@
+"""
+CS 6384 Homework 5 Programming
+Implement the __getitem__() function in this python script
+"""
+import torch
+import torch.utils.data as data
+import csv
+import os, math
+import sys
+import time
+import random
+import numpy as np
+import cv2
+import glob
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+# The dataset class
+class CrackerBox(data.Dataset):
+    def __init__(self, image_set = 'train', data_path = 'data'):
+
+        self.name = 'cracker_box_' + image_set
+        self.image_set = image_set
+        self.data_path = data_path
+        self.classes = ('__background__', 'cracker_box')
+        self.width = 640
+        self.height = 480
+        self.yolo_image_size = 448
+        self.scale_width = self.yolo_image_size / self.width
+        self.scale_height = self.yolo_image_size / self.height
+        self.yolo_grid_num = 7
+        self.yolo_grid_size = self.yolo_image_size / self.yolo_grid_num
+        # split images into training set and validation set
+        self.gt_files_train, self.gt_files_val = self.list_dataset()
+        # the pixel mean for normalization
+        self.pixel_mean = np.array([[[102.9801, 115.9465, 122.7717]]], dtype=np.float32)
+
+        # training set
+        if image_set == 'train':
+            self.size = len(self.gt_files_train)
+            self.gt_paths = self.gt_files_train
+            print('%d images for training' % self.size)
+        else:
+            # validation set
+            self.size = len(self.gt_files_val)
+            self.gt_paths = self.gt_files_val
+            print('%d images for validation' % self.size)
+
+
+    # list the ground truth annotation files
+    # use the first 100 images for training
+    def list_dataset(self):
+    
+        filename = os.path.join(self.data_path, '*.txt')
+        gt_files = sorted(glob.glob(filename))
+        
+        gt_files_train = gt_files[:100]
+        gt_files_val = gt_files[100:]
+        
+        return gt_files_train, gt_files_val
+
+
+    # TODO: implement this function
+    def __getitem__(self, idx):
+    
+        # gt file
+        filename_gt = self.gt_paths[idx]
+
+        ### ADD YOUR CODE HERE ###
+                
+        ##### Image Tensor Code Start #####
+
+        pixel_mean_tensor = torch.tensor(self.pixel_mean)
+        filename_comps = filename_gt.split("-")
+        img_filename = filename_comps[0] + ".jpg"
+        cv2_image = cv2.imread(img_filename)
+        orig_height, orig_width = cv2_image.shape[:2]
+        rescaled_cv2_image = cv2.resize(cv2_image, (self.yolo_image_size, self.yolo_image_size))
+        rescaled_height, rescaled_width = rescaled_cv2_image.shape[:2]
+        rescaled_image_tensor = torch.tensor(rescaled_cv2_image)
+        norm_image_tensor = ( rescaled_image_tensor - pixel_mean_tensor ) / 255
+        image_blob = norm_image_tensor.permute(2, 0, 1)
+
+        #print(pixel_mean_tensor)
+        #print(image)
+        #print(norm_image)
+        #print(norm_image/255)
+
+        #image = torchvision.io.read_image(img_filename)
+        #print(image)
+        #img_transform = torchvision.transforms.Resize(
+        #    (self.yolo_image_size, self.yolo_image_size)
+        #)
+        #image_blob = img_transform(image)
+        #image_blob = image_blob1 - pixel_mean_tensor.view((3,1,1))
+
+        ##### Image Tensor Code End #####
+
+        ##### Bounding Box Code Start #####
+
+        gt_box_blob_arr = np.zeros(
+            (self.yolo_grid_num, self.yolo_grid_num, 5), 
+            dtype=float
+        )
+
+        gt_mask_blob_arr = np.zeros(
+            (self.yolo_grid_num, self.yolo_grid_num), 
+            dtype=int
+        )
+
+        gt_bbox_coords = np.loadtxt(filename_gt, delimiter=' ')
+
+        resizing_factors_for_coords = np.array([
+            self.scale_width,
+            self.scale_height,
+            self.scale_width,
+            self.scale_height,
+        ])
+
+        gt_bbox_resized_coords = gt_bbox_coords * resizing_factors_for_coords
+
+        gt_cell_values = np.zeros((5,))
+        gt_cell_values[2:4] = [
+            gt_bbox_resized_coords[2] - gt_bbox_resized_coords[0],
+            gt_bbox_resized_coords[3] - gt_bbox_resized_coords[1],
+        ]
+        gt_cell_values[0:2] = [
+            gt_bbox_resized_coords[0] + gt_cell_values[2] / 2.0,
+            gt_bbox_resized_coords[1] + gt_cell_values[3] / 2.0
+        ]
+        gt_cell_values[4] = 1.0
+
+        grid_x_idx = int(gt_cell_values[0] // 64)
+        grid_y_idx = int(gt_cell_values[1] // 64)
+
+        x_offset = gt_cell_values[0] % 64
+        y_offset = gt_cell_values[1] % 64
+
+        gt_cell_values[0:2] = [
+            x_offset / self.yolo_grid_size, 
+            y_offset / self.yolo_grid_size
+        ]
+
+        gt_cell_values[2:4] /= float(self.yolo_image_size)
+
+        gt_box_blob_arr[grid_y_idx][grid_x_idx] = gt_cell_values
+        gt_mask_blob_arr[grid_y_idx][grid_x_idx] = 1
+
+        gt_box_blob = torch.tensor(gt_box_blob_arr).permute(2, 0, 1)
+        gt_mask_blob = torch.tensor(gt_mask_blob_arr)
+
+        #print(gt_bbox_coords)
+        #print(resizing_factors_for_coords)
+        #print(gt_bbox_resized_coords)
+        #print(gt_cell_values)
+        #print(grid_x_idx)
+        #print(grid_y_idx)
+        #print(self.yolo_grid_size)
+        #print(self.yolo_image_size)
+        #print(gt_box_blob.shape)
+        #print(gt_mask_blob.shape)
+
+        ##### Bounding Box Code End #####
+
+        # this is the sample dictionary to be returned from this function
+        sample = {'image': image_blob,
+                  'gt_box': gt_box_blob,
+                  'gt_mask': gt_mask_blob}
+
+        return sample
+
+
+    # len of the dataset
+    def __len__(self):
+        return self.size
+        
+
+# draw grid on images for visualization
+def draw_grid(image, line_space=64):
+    H, W = image.shape[:2]
+    image[0:H:line_space] = [255, 255, 0]
+    image[:, 0:W:line_space] = [255, 255, 0]
+
+
+# the main function for testing
+if __name__ == '__main__':
+    dataset_train = CrackerBox('train')
+    dataset_val = CrackerBox('val')
+    
+    # dataloader
+    train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=1, shuffle=False, num_workers=0)
+    
+    # visualize the training data
+    for i, sample in enumerate(train_loader):
+        
+        image = sample['image'][0].numpy().transpose((1, 2, 0))
+        gt_box = sample['gt_box'][0].numpy()
+        gt_mask = sample['gt_mask'][0].numpy()
+
+        y, x = np.where(gt_mask == 1)
+        cx = gt_box[0, y, x] * dataset_train.yolo_grid_size + x * dataset_train.yolo_grid_size
+        cy = gt_box[1, y, x] * dataset_train.yolo_grid_size + y * dataset_train.yolo_grid_size
+        w = gt_box[2, y, x] * dataset_train.yolo_image_size
+        h = gt_box[3, y, x] * dataset_train.yolo_image_size
+
+        x1 = cx - w * 0.5
+        x2 = cx + w * 0.5
+        y1 = cy - h * 0.5
+        y2 = cy + h * 0.5
+
+        print(image.shape, gt_box.shape)
+        
+        # visualization
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 3, 1)
+        im = image * 255.0 + dataset_train.pixel_mean
+        im = im.astype(np.uint8)
+        plt.imshow(im[:, :, (2, 1, 0)])
+        plt.title('input image (448x448)', fontsize = 16)
+
+        ax = fig.add_subplot(1, 3, 2)
+        draw_grid(im)
+        plt.imshow(im[:, :, (2, 1, 0)])
+        rect = patches.Rectangle((x1, y1), x2-x1, y2-y1, linewidth=2, edgecolor='g', facecolor="none")
+        ax.add_patch(rect)
+        plt.plot(cx, cy, 'ro', markersize=12)
+        plt.title('Ground truth bounding box in YOLO format', fontsize=16)
+        
+        ax = fig.add_subplot(1, 3, 3)
+        plt.imshow(gt_mask)
+        plt.title('Ground truth mask in YOLO format (7x7)', fontsize=16)
+        plt.show()
---
+++ Spring 2024/CS 6384/Homework 5/homework5_programming/yolo/model.py
@@ -0,0 +1,322 @@
+"""
+CS 6384 Homework 5 Programming
+Implement the create_modules() function in this python script
+"""
+import os
+import math
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+# the YOLO network class
+class YOLO(nn.Module):
+    def __init__(self, num_boxes, num_classes):
+        super(YOLO, self).__init__()
+        # number of bounding boxes per cell (2 in our case)
+        self.num_boxes = num_boxes
+        # number of classes for detection (1 in our case: cracker box)
+        self.num_classes = num_classes
+        self.image_size = 448
+        self.grid_size = 64
+        # create the network
+        self.network = self.create_modules()
+        
+        # initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+
+    #TODO: implement this function to build the network
+    def create_modules(self):
+        modules = nn.Sequential()
+
+        ### ADD YOUR CODE HERE ###
+        # hint: use the modules.add_module()
+        modules.add_module(
+            'conv_1', 
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=16,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_1', 
+            nn.ReLU()
+        )
+        modules.add_module(
+            'maxpool_1', 
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                dilation=1,
+                ceil_mode=False
+            )
+        )
+
+        modules.add_module(
+            'conv_2', 
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_2', 
+            nn.ReLU()
+        )
+        modules.add_module(
+            'maxpool_2', 
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                dilation=1,
+                ceil_mode=False
+            )
+        )
+
+        modules.add_module(
+            'conv_3', 
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_3', 
+            nn.ReLU()
+        )
+        modules.add_module(
+            'maxpool_3', 
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                dilation=1,
+                ceil_mode=False
+            )
+        )
+
+        modules.add_module(
+            'conv_4', 
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_4', 
+            nn.ReLU()
+        )
+        modules.add_module(
+            'maxpool_4', 
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                dilation=1,
+                ceil_mode=False
+            )
+        )
+
+        modules.add_module(
+            'conv_5', 
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=256,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_5', 
+            nn.ReLU()
+        )
+        modules.add_module(
+            'maxpool_5', 
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                dilation=1,
+                ceil_mode=False
+            )
+        )
+
+        modules.add_module(
+            'conv_6', 
+            nn.Conv2d(
+                in_channels=256,
+                out_channels=512,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_6', 
+            nn.ReLU()
+        )
+        modules.add_module(
+            'maxpool_6', 
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                dilation=1,
+                ceil_mode=False
+            )
+        )
+
+        modules.add_module(
+            'conv_7', 
+            nn.Conv2d(
+                in_channels=512,
+                out_channels=1024,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_7', 
+            nn.ReLU()
+        )
+
+        modules.add_module(
+            'conv_8', 
+            nn.Conv2d(
+                in_channels=1024,
+                out_channels=1024,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_8', 
+            nn.ReLU()
+        )
+
+        modules.add_module(
+            'conv_9', 
+            nn.Conv2d(
+                in_channels=1024,
+                out_channels=1024,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+        modules.add_module(
+            'relu_9', 
+            nn.ReLU()
+        )
+
+        modules.add_module(
+            'flatten', 
+            nn.Flatten(start_dim=1, end_dim=-1)
+        )
+
+        modules.add_module(
+            'fc1',
+            nn.Linear(
+                in_features=50176, 
+                out_features=256, 
+                bias=True
+            )
+        )
+
+        modules.add_module(
+            'fc2',
+            nn.Linear(
+                in_features=256, 
+                out_features=256, 
+                bias=True
+            )
+        )
+
+        modules.add_module(
+            'output',
+            nn.Linear(
+                in_features=256, 
+                out_features=539, 
+                bias=True
+            )
+        )
+
+        modules.add_module(
+            'sigmoid',
+            nn.Sigmoid()
+        )
+
+        return modules
+
+
+    # output (batch_size, 5*B + C, 7, 7)
+    # In the network output (cx, cy, w, h) are normalized to be [0, 1]
+    # This function undo the noramlization to obtain the bounding boxes in the orignial image space
+    def transform_predictions(self, output):
+        batch_size = output.shape[0]
+        x = torch.linspace(0, 384, steps=7)
+        y = torch.linspace(0, 384, steps=7)
+        corner_x, corner_y = torch.meshgrid(x, y, indexing='xy')
+        corner_x = torch.unsqueeze(corner_x, dim=0)
+        corner_y = torch.unsqueeze(corner_y, dim=0)
+        corners = torch.cat((corner_x, corner_y), dim=0)
+        # corners are top-left corners for each cell in the grid
+        corners = corners.unsqueeze(0).repeat(batch_size, 1, 1, 1)
+        pred_box = output.clone()
+
+        # for each bounding box
+        for i in range(self.num_boxes):
+            # x and y
+            pred_box[:, i*5, :, :] = corners[:, 0, :, :] + output[:, i*5, :, :] * self.grid_size
+            pred_box[:, i*5+1, :, :] = corners[:, 1, :, :] + output[:, i*5+1, :, :] * self.grid_size
+            # w and h
+            pred_box[:, i*5+2, :, :] = output[:, i*5+2, :, :] * self.image_size
+            pred_box[:, i*5+3, :, :] = output[:, i*5+3, :, :] * self.image_size
+
+        return pred_box
+
+
+    # forward pass of the YOLO network
+    def forward(self, x):
+        # raw output from the network
+        output = self.network(x).reshape((-1, self.num_boxes * 5 + self.num_classes, 7, 7))
+        # compute bounding boxes in the original image space
+        pred_box = self.transform_predictions(output)
+        return output, pred_box
+
+
+# run this main function for testing
+if __name__ == '__main__':
+    network = YOLO(num_boxes=2, num_classes=1)
+    print(network)
+
+    image = np.random.uniform(-0.5, 0.5, size=(1, 3, 448, 448)).astype(np.float32)
+    image_tensor = torch.from_numpy(image)
+    print('input image:', image_tensor.shape)
+
+    output, pred_box = network(image_tensor)
+    print('network output:', output.shape, pred_box.shape)
